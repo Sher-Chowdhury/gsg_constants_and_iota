226bebe76415718fee1ae2e4ac580eba70175b38
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-09-14 14:39:34 +0100

    wip

--- main.go
+++ main.go
@@ -2,24 +2,22 @@ package main
 
 import "fmt"
 
+// the 'iota' key word acts like a counter starting from
+const (
+	alpha   = iota
+	bravo   = iota
+	charlie = iota
+)
+
+// the iota reset to 0 when moving to the next constang block
+const (
+	delta   = iota
+	echono  = iota
+	foxtrot = iota
+)
+
 func main() {
 
-	// the 'iota' key word acts like a counter starting from
-	const (
-		alpha   = iota
-		bravo   = iota
-		charlie = iota
-	)
-
 	fmt.Println(alpha, bravo, charlie)
-
-	// the iota reset to 0 when moving to the next constang block
-	const (
-		delta   = iota
-		echono  = iota
-		foxtrot = iota
-	)
-
 	fmt.Println(delta, echono, foxtrot)
-
 }
